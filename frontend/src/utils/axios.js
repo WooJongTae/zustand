f9a7f8f2c949c1ed3aca7c54298e56a64b6d25ac
woojongtae <binily@naver.com>
2024-01-26 13:19:24 +0900

    axios test

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://test-back-96q5.onrender.com"
+    ? "https://server-ij7f.onrender.com"
     : "http://localhost:4000",
 });
 
