51feafa2e4f4efe92cb7e84c3cd06b2cac0516e4
woojongtae <binily@naver.com>
2024-02-10 23:50:53 +0900

    css add

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const HomePage = () => {
@@ -8,10 +8,17 @@ const HomePage = () => {
     register,
     handleSubmit,
     reset,
+    setFocus,
+    setState,
+    watch,
+    getValues,
     formState: { errors },
-  } = useForm({ mode: "onBlur" });
+  } = useForm({ mode: "onBlur " });
+
+  const watchForm = watch("email");
+  console.log(watch());
   console.log(errors);
-  console.log(register);
+
   const emailRequired = {
     required: " 이메일은 필수 입력입니다.",
     pattern: {
@@ -19,43 +26,57 @@ const HomePage = () => {
       message: "이메일 형식에 맞지 않습니다.",
     },
   };
+  const onSubmit = (data) => {
+    console.log("Form submitted.", data);
+  };
 
   const handleFocus = () => {
     setFocused(true);
   };
 
   const handleBlur = () => {
-    setFocused(false);
+    console.log(567);
   };
-  console.log(focused);
   return (
     <div>
-      <div>
+      <div className="">
+        <div className=" overflow-hidden lg:max-h-[700px] relative">
+          <img
+            className="  object-cover scale-125 -translate-y-[10%]  min-h-[350px] sm:min-h-[480px] lg:min-h-[43.75rem] "
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/c2e7a54c-765d-48c8-a9ef-91990cacba49/KR-ko-20240205-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+            alt="배경 이미지"
+          />
+        </div>
+        <div className="bg-gradient-to-t from-black/80 to-black/80 absolute z-[2] top-0 left-0 w-full min-h-[350px] sm:min-h-[480px] lg:min-h-[43.75rem] rgba(0,0,0,0.8)">
+          safasfasfafas
+        </div>
         <h1>영화, 시리즈 등을 무제한으로</h1>
         <p>어디서나 자유롭게 시청하세요. 해지는 언제든 가능합니다.</p>
         <p>
           시청할 준비가 되셨나요? 멤버쉽 등록하거나 재시작하려면 이메일 주소를
           입력해주세요.
         </p>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className=" relative">
             <label
-              className={`absolute top-1/2 transform   ${
-                focused
-                  ? " text-xs  -translate-y-6 left-5"
+              className={`absolute top-1/2 transform transition-all duration-200
+              ${
+                watchForm
+                  ? " text-xs  -translate-y-6 left-5 "
                   : "-translate-y-1/2 left-5"
-              }`}
+              }              `}
             >
               이메일 주소
             </label>
             <input
               className="px-4 py-3 border-4 border-black  "
               onFocus={handleFocus}
-              onBlur={handleBlur}
+              //   onBlur={handleBlur}
               {...register("email", emailRequired)}
             />
           </div>
-          <button>시작하기</button>
+          {errors.email && <p>{errors.email.message}</p>}
+          <button type="submit">시작하기</button>
         </form>
       </div>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    // screens: {
+    //   tablet: "960px",
+    // },
     extend: {},
   },
   plugins: [],
